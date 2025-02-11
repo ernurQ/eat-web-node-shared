5e0c8a22455fe868bab1aaf88a868c5810a31afc
enrur1 <garifullin.ernur@gmail.com>
2025-02-11 19:56:50 +0500

    feat: add error log to error handler

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,12 +1,13 @@
 import { ErrorRequestHandler } from 'express'
 import { BaseError } from '../errors'
 
-export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+export const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
 	if (err instanceof BaseError) {
 		res.status(err.status).send(err.serializeError())
 		return
 	}
 
+	console.error(err)
 	const internalServerError = new BaseError(500, 'Something went wrong.')
 	res.status(500).send(internalServerError.serializeError())
 }
